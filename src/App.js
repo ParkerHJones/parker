292aafaa1765019c4e7a3ea7e5e731c781672b82
Parker Jones <74691370+ParkerHJones@users.noreply.github.com>
2021-06-02 13:02:24 -0600

    updated navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home';
 import Footer from './components/Footer'; 
 import About from './components/About'; 
+import NavBar from './components/NavBar'; 
 import './App.css';
 
 function App() {
   return (
       <Router>   
     <div className="app">
+      <NavBar/>
     <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/about" component={About} />
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "./style.css"; 
+
+const NavBar = () => {
+    return <nav className="navbar">
+    <div className="nav-wrapper">
+    <div className="logo">
+    </div>
+        <div className="list-wrapper">
+     <ul>
+         <li>
+             <Link to="/" > Home </Link>
+         </li>
+         <li>
+             <Link to="/about" > About </Link>
+         </li>
+         <li>
+             <Link to="/education"> Education</Link>
+         </li>
+         <li>
+             <Link to="/resume" > Resume </Link>
+         </li>
+         <li>
+             <Link to="/skills" > Skills </Link>
+         </li>
+         <li>
+             <Link to="/contact" > Contact </Link>
+         </li>
+     </ul>
+        </div>
+    </div>
+    </nav>; 
+};
+
+export default NavBar; 
