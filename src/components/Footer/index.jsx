9d53330a3f598ed8c1d142e4c4d060b57fb71e3d
Parker Jones <74691370+ParkerHJones@users.noreply.github.com>
2021-06-27 12:44:57 -0600

    added youtube

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -19,6 +19,15 @@ const Footer  = () => {
             src={`${process.env.PUBLIC_URL}/tiktok2.jpeg`}
             alt="LinkIn-icon"
             />
+            <a
+            href="https://www.youtube.com/channel/UCpfi1jp0GX4l_Abf0NkzZrw"
+            target="_blank"
+            >
+            <img
+             src={`${process.env.PUBLIC_URL}/youtubelogo.png`}
+             alt="LinkIn-icon"
+             />
+            </a>
             </a>
         </footer>
     );
